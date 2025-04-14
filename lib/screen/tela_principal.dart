bc46ae154b37fd92b71ec7e60ceec8d1ba9ff30f
André Belarmino Rodrigues da Rocha <andre.dev.rocha@gmail.com>
2025-04-14 18:59:03 -0300

    tela principal terminada

--- lib/screen/tela_principal.dart
+++ lib/screen/tela_principal.dart
@@ -5,23 +5,120 @@ import 'package:netflix_clone/screen/tela1.dart';
 class TelaPrincipal extends StatelessWidget {
   const TelaPrincipal({super.key});
 
+  final String privacidade = 'https://help.netflix.com/legal/privacy';
+
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length:3, child: Scaffold(
-      bottomNavigationBar: Container(
-        color:Colors.black,
-        height: 70,
-        child: TabBar(tabs: [
-          Tab(icon: Icon(Iconsax.home5),text: "Inicio"),
-          Tab(icon:Icon(Iconsax.search_normal), text:"Procurar"),
-          Tab(icon: Icon(Icons.photo_library_outlined), text:"Novidades")        ],
-          indicatorColor: Colors.grey,
-          labelColor: Colors.white,
-          unselectedLabelColor: Colors.grey,),
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset("assets/netflixbackground.jpg", fit: BoxFit.cover),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.transparent, Color.fromRGBO(0, 0, 0, 0.9)],
+              ),
+            ),
+            child: Container(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Image.asset("assets/netflix_logo.png", height: 140),
+                      Spacer(),
+                        Text(
+                          "Privacidade",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      SizedBox(width: 20),
+                      Text(
+                        "Entrar",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      PopupMenuButton(
+                        icon: Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                        itemBuilder:
+                            (BuildContext context) => <PopupMenuEntry<String>>[
+                              const PopupMenuItem<String>(
+                                value: 'PERGUNTAS FREQUENTES',
+                                child: Text('PERGUNTAS FREQUENTES'),
+                              ),
+                              const PopupMenuItem<String>(
+                                value: 'AJUDA',
+                                child: Text('AJUDA'),
+                              ),
+                            ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 350),
+                  Container(
+                    width: 250,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          textAlign: TextAlign.center,
+                          "Filmes, séries e muito mais, sem limites",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 37,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          textAlign: TextAlign.center,
+                          "Assista onde quiser. Cancele quando quiser.",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 45),
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.symmetric(horizontal: 5),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(255, 0, 0, 1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0),
+                        ),
+                      ),
+                      onPressed: (){},
+                      child: Text(
+                        "Vamos lá",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-      body: TabBarView(children: [
-        Tela1(), Scaffold(), Scaffold()
-      ],),
-    ),);
+    );
   }
 }
